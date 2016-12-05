8c682e6e083699f2529385d1294acf09a3433cce
Emilio Garcia-Fidalgo <emilio.garcia@uib.es>
2016-12-05 17:29:22 +0100

    Reading calibration parameters from topic

--- src/nodes/ibvs.cpp
+++ src/nodes/ibvs.cpp
@@ -1,6 +1,68 @@
-#include <cstdio>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <ros/ros.h>
+#include <sensor_msgs/RegionOfInterest.h>
+
+class IBVS
+{
+public:
+	IBVS() :
+			nh("~"),
+			it(nh)
+	{
+        // Reading calibration information
+        ROS_INFO("Waiting for calibration information (10s) ...");
+
+        boost::shared_ptr<sensor_msgs::CameraInfo const> sp;
+        sp = ros::topic::waitForMessage<sensor_msgs::CameraInfo>("camera_info", ros::Duration(10));
+
+        if (sp == NULL)
+        {
+            // No calibration received
+            ROS_FATAL("No calibration received");
+            ros::shutdown();
+        }
+        else
+        {
+            // Processing the received calibration
+            sensor_msgs::CameraInfo cinfo = *sp;
+
+            ROS_INFO("Calibration data:");
+
+            width = cinfo.width;
+            ROS_INFO("Image width: %u", width);
+
+            height = cinfo.height;
+            ROS_INFO("Image height: %u", height);
+
+            fs = cinfo.K[0];
+            ROS_INFO("Focal length: %.2f", fs);
+
+            u0 = cinfo.K[2];
+            v0 = cinfo.K[5];
+            ROS_INFO("Principal point: (%.2f, %.2f)", u0, v0);
+        }
+	}
+
+private:
+	ros::NodeHandle nh;
+	image_transport::ImageTransport it;
+
+    // Calibration info
+    unsigned width;
+    unsigned height;
+    double fs;
+    double u0;
+    double v0;
+};
 
 int main(int argc, char** argv)
 {
+	// ROS
+	ros::init(argc, argv, "ibvs");
+    ROS_INFO("MERBOTS Image-Based Visual Servoing");
+	IBVS ibvs;
+	ros::spin();
+
 	return 0;
 }
