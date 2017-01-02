36a0c9a9375faea154f0a901fcdaa93060986158
Emilio Garcia-Fidalgo <emilio.garcia@uib.es>
2017-01-02 12:20:53 +0100

    Fixing problem with image encoding

--- src/nodes/ibvs.cpp
+++ src/nodes/ibvs.cpp
@@ -221,7 +221,7 @@ public:
         cv_bridge::CvImageConstPtr cv_ptr;
         try
         {
-            cv_ptr = cv_bridge::toCvShare(msg, msg->encoding);
+            cv_ptr = cv_bridge::toCvShare(msg, "bgr8");
         }
         catch (cv_bridge::Exception& e)
         {
